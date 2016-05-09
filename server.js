71a54a3e63bd09f229eedd4371735044e6a8ea0d
spacelab2 <infosuemcmahon@gmail.com>
2016-05-09 22:02:15 +0100

    Initial commit. Set up local host.

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (request, response) {
+    if (request.url === '/') {
+        response.setHeader('Content-Type', 'text/html');
+        fs.createReadStream('index.html').pipe(response);
+    }
+});
+
+server.listen(8080, function () {
+    console.log('Im listening on port 8080');
+});
